be4806d3c653f40c1981bd45b69c71a54b52ae7f
mustela <ejankowski@gmail.com>
2015-10-15 16:19:22 +0200

    Adding stats server

--- lib/stats_collector/configuration.rb
+++ lib/stats_collector/configuration.rb
@@ -7,11 +7,12 @@ module StatsCollector
 
     attr_accessor :prefix, :statsd_address, :sample_rate
 
-    # validates! :prefix, presence: true
+    validates! :prefix, presence: true
 
     def setup
       valid?
-      StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new(statsd_address, :statsd) if statsd_address
+      address = @statsd_address || ENV['STATSD_ADDR']
+      StatsD.backend = StatsD::Instrument::Backends::UDPBackend.new(address, :statsd) if address
       StatsD.prefix = prefix
       StatsD.default_sample_rate = (ENV['STATSD_SAMPLE_RATE'] || sample_rate || 1).to_f
     end
