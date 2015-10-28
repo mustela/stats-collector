e7fa07ecdfea49fa933bc5c0ede6b62a8a6ba156
mustela <ejankowski@gmail.com>
2015-10-28 11:51:00 +0100

    Removing http code tracking since nunes already manage that

--- lib/stats_collector.rb
+++ lib/stats_collector.rb
@@ -7,17 +7,16 @@ require 'stats_collector/director'
 require 'stats_collector/stats_monitor'
 require 'stats_collector/event'
 
-
 module StatsCollector
   class << self
     def configure
       yield(configuration)
-      require 'pp'; pp configuration
       configuration.setup
-      if configuration.stats_enabled
-        Director.play
-        require 'stats_collector/railtie' if defined? Rails
-      end
+      return configuration unless configuration.stats_enabled
+
+      Director.play
+      require 'stats_collector/railtie' if defined? Rails
+      configuration
     end
 
     def configuration
--- lib/stats_collector/director.rb
+++ lib/stats_collector/director.rb
@@ -1,29 +1,8 @@
 module StatsCollector
   module Director
-    STATSD_REQUEST_METRICS = {
-      'request.success'               => 200,
-      'request.redirect'              => 302,
-      'request.bad_request'           => 400,
-      'request.not_found'             => 404,
-      'request.too_many_requests'     => 429,
-      'request.internal_server_error' => 500,
-      'request.bad_gateway'           => 502
-    }.freeze
-
     class << self
       def play
         Nunes.subscribe(::StatsCollector::Adapters::StatsInstrument.new)
-        track_requests
-      end
-
-      def track_requests
-        StatsdMonitor.extend(::StatsD::Instrument)
-
-        STATSD_REQUEST_METRICS.each do |name, code|
-          StatsdMonitor.statsd_count_if(:call, name) do |status, _env, _body|
-            status.to_i == code
-          end
-        end
       end
     end
   end
